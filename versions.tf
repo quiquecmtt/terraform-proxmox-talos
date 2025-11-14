c8eb58f736729b2b229acd4e6e924e9c251443f1
Enrique Cametti <44720459+quiquecmtt@users.noreply.github.com>
2025-11-14 00:13:00 -0300

    chore(deps): update all non-major dependencies (#6)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- versions.tf
+++ versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.80.0"
+      version = "0.86.0"
     }
     talos = {
       source  = "siderolabs/talos"
-      version = "0.9.0-alpha.0"
+      version = "0.9.0"
     }
   }
 }
